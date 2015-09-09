4781c1f7000e52de504c5bb80d45e97c1931e6e0
Terence Lim <terence9213@hotmail.com>
2015-09-09 23:12:12 +0800

    New file created
    Created a new file just to test this thingy and commit.
    Done in Notepad++

---
+++ test/test.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class test
+{
+	public static void main(String[] args)
+	{
+		ArrayList<String> listOne = new ArrayList<String>();
+		
+		listOne.add("one");
+		listOne.add("two");
+		
+		List newList = listOne.subList(0,1);
+		
+		System.out.println(listOne);
+		System.out.println(newList);
+		
+		ArrayList<String> listTwo = new ArrayList<String>();
+		listTwo = new ArrayList<String>(newList);
+		System.out.println(listTwo);
+		
+	}
+}
